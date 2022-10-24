b075b85442db1d3692cbe1b996f50dc5d405bcd5
wyyden <ankudinoff.daniil@yandex.ru>
2022-10-24 18:58:20 +0300

    some fix

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,63 @@
 import java.util.Arrays;
 import java.util.Scanner;
-
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 class Main{
+    public static String readFile(String path, Charset encoding) throws IOException {
+        String content = Files.lines(Paths.get(path), encoding)
+                .collect(Collectors.joining(System.lineSeparator()));
+
+        return content;
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        char[] array = sc.nextLine().toLowerCase().replaceAll(" ", "").toCharArray();
-        Arrays.sort(array);
-        int max = 0;
-        int count = 1;
-        char symbol = ' ';
-        for (int i=0;i<array.length;i++){
-            for (int j=i+1;j<array.length;j++){
-                if (array[i] == array[j]) count++;
+        System.out.println("Прочитать из файла - 1; \nВвести вручную - 2\n");
+        int answer = sc.nextInt();
+        String content = null;
+        if (answer == 1){
+            String filePath = "01.txt";
+
+            try {
+                content = readFile(filePath, StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            System.out.println(array[i] + ": " + count);
-            count = 1;
         }
-        System.out.println(symbol + " " + max);
+        else {
+            content = sc.nextLine();
+        }
+        char[] array = content.toLowerCase().replaceAll("[^A-Za-zА-Яа-я0-9]", "").toCharArray();
+        Arrays.sort(array);
+        int count = 1;
+        int countAll = 0;
+        char symbol = ' ';
+        for (int i=0; i<array.length-1; i++) {
+            if ((i != array.length - 1) && (array[i] == array[i + 1])){
+                if (i + 1 == array.length - 1) {
+                    count++;
+                    System.out.println(array[i] + ": " + count);
+                    countAll++;
+                    break;
+                }
+            count++;
+            }
+            else if ((i+1 == array.length-1) && (array[i] != array[i + 1])){
+                System.out.println(array[i] + ": " + count);
+                count = 1;
+                System.out.println(array[i+1] + ": " + count);
+                countAll += 2;
+                break;
+            }
+            else{
+                System.out.println(array[i] + ": " + count);
+                count = 1;
+            }
+            countAll++;
+        }
+        System.out.println("Общее кол-во = " + countAll);
     }
 }
